a49c7dc9df861fee7956ff49ee4fd4b0de839ee4
poledna0 <henriquepoledna@proton.me>
2026-02-25 14:11:40 -0300

    removendo usar cpu

--- training/main.py
+++ training/main.py
@@ -28,8 +28,9 @@ BATCH_SIZE = 32
 EPOCHS = 300
 LR = 1e-4
 
-DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-print("Usando device:", DEVICE)
+DEVICE = "cuda" 
+if torch.cuda.is_available():
+    print("Usando device:", DEVICE)
 
 class ASVspoofCMDataset(Dataset):
 
