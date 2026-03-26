91be0370be3e76b1577122e76bfc85df061417fc
poledna0 <henriquepoledna@proton.me>
2026-03-26 10:26:19 -0300

    feat: teste de novo modelo resnet e removendo LFCC de treinamento por baixo rendimento

--- training/lab.sh
+++ training/lab.sh
@@ -39,12 +39,3 @@ python3 /home/henrique/pibic/git/ASVspoof-Spectrogram/training/m-ResNet-18.py \
 --protocol_eval /home/henrique/pibic/git/ASVspoof-Spectrogram/PA_cm_protocols/ASVspoof2019.PA.cm.eval.trl.txt \
 --model_name cqcc 2>&1 | tee log_cqcc.txt
 
-
-python3 /home/henrique/pibic/git/ASVspoof-Spectrogram/training/m-ResNet-18.py \
---img_dir_train /home/henrique/pibic/data-set-asv/lfcc/PA/ASVspoof2019_PA_train/flac \
---img_dir_dev /home/henrique/pibic/data-set-asv/lfcc/PA/ASVspoof2019_PA_dev/flac \
---img_dir_eval /home/henrique/pibic/data-set-asv/lfcc/PA/ASVspoof2019_PA_eval/flac \
---protocol_train /home/henrique/pibic/git/ASVspoof-Spectrogram/PA_cm_protocols/ASVspoof2019.PA.cm.train.trn.txt \
---protocol_dev /home/henrique/pibic/git/ASVspoof-Spectrogram/PA_cm_protocols/ASVspoof2019.PA.cm.dev.trl.txt \
---protocol_eval /home/henrique/pibic/git/ASVspoof-Spectrogram/PA_cm_protocols/ASVspoof2019.PA.cm.eval.trl.txt \
---model_name lfcc 2>&1 | tee log_lfcc.txt
