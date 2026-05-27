a9225269bb9ba7b5bc93f335649b5ac3d3d21eff
poledna0 <henriquepoledna@proton.me>
2026-05-27 17:53:21 -0300

    att script

--- teste.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-python3 /home/henrique/pibic/git/ASVspoof-Spectrogram/training/padrao-ResNet-18.py \
---img_dir_train /home/henrique/pibic/espectogramas_extremos/logmel \
---img_dir_dev /home/henrique/pibic/espectogramas_extremos/logmel \
---img_dir_eval /home/henrique/pibic/espectogramas_extremos/logmel \
---protocol_train /home/henrique/pibic/git/ASVspoof-Spectrogram/PA_cm_protocols/ASVspoof2019.PA.cm.train.trn.txt \
---protocol_dev /home/henrique/pibic/git/ASVspoof-Spectrogram/PA_cm_protocols/ASVspoof2019.PA.cm.dev.trl.txt \
---protocol_eval /home/henrique/pibic/git/ASVspoof-Spectrogram/PA_cm_protocols/ASVspoof2019.PA.cm.eval.trl.txt \
---model_name logmel 2>&1 | tee log_logmel.txt
--- training/lab.sh
+++ training/lab.sh
@@ -1,38 +1,37 @@
 #!/bin/bash
 
 python3 /home/henrique/pibic/git/ASVspoof-Spectrogram/training/padrao-ResNet-18.py \
---img_dir_train /home/henrique/pibic/espectogramas_extremos/logmel \
---img_dir_dev /home/henrique/pibic/espectogramas_extremos/logmel \
---img_dir_eval /home/henrique/pibic/espectogramas_extremos/logmel \
+--img_dir_train /home/henrique/pibic/espectogramas_extremos/logmel/PA/ASVspoof2019_PA_train/flac \
+--img_dir_dev /home/henrique/pibic/espectogramas_extremos/logmel/PA/ASVspoof2019_PA_dev/flac \
+--img_dir_eval /home/henrique/pibic/espectogramas_extremos/logmel/PA/ASVspoof2019_PA_eval/flac \
 --protocol_train /home/henrique/pibic/git/ASVspoof-Spectrogram/PA_cm_protocols/ASVspoof2019.PA.cm.train.trn.txt \
 --protocol_dev /home/henrique/pibic/git/ASVspoof-Spectrogram/PA_cm_protocols/ASVspoof2019.PA.cm.dev.trl.txt \
 --protocol_eval /home/henrique/pibic/git/ASVspoof-Spectrogram/PA_cm_protocols/ASVspoof2019.PA.cm.eval.trl.txt \
 --model_name logmel 2>&1 | tee log_logmel.txt
 
 python3 /home/henrique/pibic/git/ASVspoof-Spectrogram/training/padrao-ResNet-18.py \
---img_dir_train /home/henrique/pibic/espectogramas_extremos/stft \
---img_dir_dev /home/henrique/pibic/espectogramas_extremos/stft \
---img_dir_eval /home/henrique/pibic/espectogramas_extremos/stft \
+--img_dir_train /home/henrique/pibic/espectogramas_extremos/stft/PA/ASVspoof2019_PA_train/flac \
+--img_dir_dev /home/henrique/pibic/espectogramas_extremos/stft/PA/ASVspoof2019_PA_dev/flac \
+--img_dir_eval /home/henrique/pibic/espectogramas_extremos/stft/PA/ASVspoof2019_PA_eval/flac \
 --protocol_train /home/henrique/pibic/git/ASVspoof-Spectrogram/PA_cm_protocols/ASVspoof2019.PA.cm.train.trn.txt \
 --protocol_dev /home/henrique/pibic/git/ASVspoof-Spectrogram/PA_cm_protocols/ASVspoof2019.PA.cm.dev.trl.txt \
 --protocol_eval /home/henrique/pibic/git/ASVspoof-Spectrogram/PA_cm_protocols/ASVspoof2019.PA.cm.eval.trl.txt \
 --model_name stft 2>&1 | tee log_stft.txt
 
 python3 /home/henrique/pibic/git/ASVspoof-Spectrogram/training/padrao-ResNet-18.py \
---img_dir_train /home/henrique/pibic/espectogramas_extremos/cqt \
---img_dir_dev /home/henrique/pibic/espectogramas_extremos/cqt \
---img_dir_eval /home/henrique/pibic/espectogramas_extremos/cqt \
+--img_dir_train /home/henrique/pibic/espectogramas_extremos/cqt/PA/ASVspoof2019_PA_train/flac \
+--img_dir_dev /home/henrique/pibic/espectogramas_extremos/cqt/PA/ASVspoof2019_PA_dev/flac \
+--img_dir_eval /home/henrique/pibic/espectogramas_extremos/cqt/PA/ASVspoof2019_PA_eval/flac \
 --protocol_train /home/henrique/pibic/git/ASVspoof-Spectrogram/PA_cm_protocols/ASVspoof2019.PA.cm.train.trn.txt \
 --protocol_dev /home/henrique/pibic/git/ASVspoof-Spectrogram/PA_cm_protocols/ASVspoof2019.PA.cm.dev.trl.txt \
 --protocol_eval /home/henrique/pibic/git/ASVspoof-Spectrogram/PA_cm_protocols/ASVspoof2019.PA.cm.eval.trl.txt \
 --model_name cqt 2>&1 | tee log_cqt.txt
 
 python3 /home/henrique/pibic/git/ASVspoof-Spectrogram/training/padrao-ResNet-18.py \
---img_dir_train /home/henrique/pibic/espectogramas_extremos/cqcc \
---img_dir_dev /home/henrique/pibic/espectogramas_extremos/cqcc \
---img_dir_eval /home/henrique/pibic/espectogramas_extremos/cqcc \
+--img_dir_train /home/henrique/pibic/espectogramas_extremos/cqcc/PA/ASVspoof2019_PA_train/flac \
+--img_dir_dev /home/henrique/pibic/espectogramas_extremos/cqcc/PA/ASVspoof2019_PA_dev/flac \
+--img_dir_eval /home/henrique/pibic/espectogramas_extremos/cqcc/PA/ASVspoof2019_PA_eval/flac \
 --protocol_train /home/henrique/pibic/git/ASVspoof-Spectrogram/PA_cm_protocols/ASVspoof2019.PA.cm.train.trn.txt \
 --protocol_dev /home/henrique/pibic/git/ASVspoof-Spectrogram/PA_cm_protocols/ASVspoof2019.PA.cm.dev.trl.txt \
 --protocol_eval /home/henrique/pibic/git/ASVspoof-Spectrogram/PA_cm_protocols/ASVspoof2019.PA.cm.eval.trl.txt \
 --model_name cqcc 2>&1 | tee log_cqcc.txt
-
